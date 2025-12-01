0d76b282c771f00a806ac80cdc56c97cc0e57975
orosmatthew <mjo1@clevelandmetroparks.com>
2025-12-01 12:33:22 -0500

    Add day 1 part 2

---
+++ day01-part2/main.cpp
@@ -0,0 +1,34 @@
+#include "../utils.hpp"
+
+int solve(const std::string& data)
+{
+    int count = 0;
+    int floor = 0;
+    for (size_t pos = 0; pos < data.length(); ++pos) {
+        switch (data[pos]) {
+        case '(':
+            ++floor;
+            break;
+        case ')':
+            --floor;
+            break;
+        default:
+            break;
+        }
+        ++count;
+        if (floor == -1) {
+            break;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    const std::string data = read_file("./day01-part2/input.txt");
+#ifdef BENCHMARK
+    benchmark([&] { return solve(data); }, 1000000);
+#else
+    std::println("{}", solve(data));
+#endif
+}

c09b7d63c6953c0ed83ac61f9d215c1044a91568
orosmatthew <mjo1@clevelandmetroparks.com>
2025-12-01 14:18:32 -0500

    Add day 5 part 2

---
+++ day05-part2/main.cpp
@@ -0,0 +1,50 @@
+#include "../utils.hpp"
+
+static bool is_nice(const std::string& string)
+{
+    bool found = false;
+    for (size_t i = 2; i < string.length(); ++i) {
+        if (string[i] == string[i - 2]) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        return false;
+    }
+    for (size_t i = 1; i < string.length() - 2; ++i) {
+        for (size_t j = i + 2; j < string.length(); ++j) {
+            if (string[i - 1] == string[j - 1] && string[i] == string[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+static int solve(const std::string& data)
+{
+    int nice_count = 0;
+    std::string string;
+    for (size_t pos = 0; pos < data.length(); ++pos) {
+        string.clear();
+        while (data[pos] != '\n') {
+            string.push_back(data[pos]);
+            ++pos;
+        }
+        if (is_nice(string)) {
+            ++nice_count;
+        }
+    }
+    return nice_count;
+}
+
+int main()
+{
+    const std::string data = read_file("./day05-part2/input.txt");
+#ifdef BENCHMARK
+    benchmark([&] { return solve(data); }, 100000);
+#else
+    std::println("{}", solve(data));
+#endif
+}

3998750ef485a7552b96e05630120ec00b15626a
orosmatthew <mjo1@clevelandmetroparks.com>
2025-12-01 13:52:46 -0500

    Add day 4 part 2

---
+++ day04-part2/main.cpp
@@ -0,0 +1,52 @@
+#include "../utils.hpp"
+
+#include <md5.h>
+
+#include <format>
+
+static std::string parse_key(const std::string& data)
+{
+    std::string key;
+    for (size_t i = 0; i < data.length(); ++i) {
+        if (data[i] == '\n') {
+            break;
+        }
+        key.push_back(data[i]);
+    }
+    return key;
+}
+
+static bool starts_with_6_zeros(const std::string& string)
+{
+    for (size_t i = 0; i < 6; ++i) {
+        if (string[i] != '0') {
+            return false;
+        }
+    }
+    return true;
+}
+
+static int solve(const std::string& data)
+{
+    const std::string key = parse_key(data);
+    MD5 md5;
+    int count = 0;
+    while (true) {
+        const std::string input = std::format("{}{}", key, count);
+        if (const std::string hash = md5(input); starts_with_6_zeros(hash)) {
+            break;
+        }
+        ++count;
+    }
+    return count;
+}
+
+int main()
+{
+    const std::string data = read_file("./day04-part2/input.txt");
+#ifdef BENCHMARK
+    benchmark([&] { return solve(data); }, 10);
+#else
+    std::println("{}", solve(data));
+#endif
+}
